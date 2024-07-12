bc852f9937750b3d95b35973ec04ca8663f1271c
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2024-07-12 02:54:24 +0200

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,16 +2,19 @@
 
 const request = require('request');
 
+// Function to fetch and print character names in exact order
+const exactOrder = (actors, index) => {
+  if (index === actors.length) return;
+  request(actors[index], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, index + 1);
+  });
+};
+
+// Main function to fetch the movie data
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
   if (err) throw err;
   const actors = JSON.parse(body).characters;
   exactOrder(actors, 0);
 });
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
-  });
-};
